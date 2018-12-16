1b38d9d07ee691eddab56bb4e953f68916f7067e
gnosis23 <bj050323@gmail.com>
2018-12-16 18:31:35 +0800

    打包优化

--- webpack.config.js
+++ webpack.config.js
@@ -192,8 +192,39 @@ module.exports = {
 
     new webpack.DefinePlugin({
       DEBUG: JSON.stringify(!!dev)
-    })
+    }),
+
+    /*
+    使用文件路径的 hash 作为 moduleId。
+    虽然我们使用 [chunkhash] 作为 chunk 的输出名，但仍然不够。
+    因为 chunk 内部的每个 module 都有一个 id，webpack 默认使用递增的数字作为 moduleId。
+    如果引入了一个新文件或删掉一个文件，可能会导致其他文件的 moduleId 也发生改变，
+    那么受影响的 module 所在的 chunk 的 [chunkhash] 就会发生改变，导致缓存失效。
+    因此使用文件路径的 hash 作为 moduleId 来避免这个问题。
+    */
+    new webpack.HashedModuleIdsPlugin()
   ],
+
+  optimization: {
+    /*
+    上面提到 chunkFilename 指定了 chunk 打包输出的名字，那么文件名存在哪里了呢？
+    它就存在引用它的文件中。这意味着一个 chunk 文件名发生改变，会导致引用这个 chunk 文件也发生改变。
+
+    runtimeChunk 设置为 true, webpack 就会把 chunk 文件名全部存到一个单独的 chunk 中，
+    这样更新一个文件只会影响到它所在的 chunk 和 runtimeChunk，避免了引用这个 chunk 的文件也发生改变。
+    */
+    runtimeChunk: true,
+
+    splitChunks: {
+      /*
+      默认 entry 的 chunk 不会被拆分
+      因为我们使用了 html-webpack-plugin 来动态插入 <script> 标签，entry 被拆成多个 chunk 也能自动被插入到 html 中，
+      所以我们可以配置成 all, 把 entry chunk 也拆分了
+      */
+      chunks: 'all'
+    }
+  },
+
   /*
   配置开发时用的服务器，让你可以用 http://127.0.0.1:8080/ 这样的 url 打开页面来调试
   并且带有热更新的功能，打代码时保存一下文件，浏览器会自动刷新。比 nginx 方便很多
